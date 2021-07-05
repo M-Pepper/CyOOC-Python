a18596c76a45733775b73c96082427170f62f54f
M-Pepper <46696465+M-Pepper@users.noreply.github.com>
2021-07-05 15:12:37 -0400

    Update def tribonacci_sequence.py

--- def tribonacci_sequence.py
+++ def tribonacci_sequence.py
@@ -22,3 +22,17 @@ def tribonacci(signature, n):
             tribonacci_list.append(tribonacci_list[i-1] + tribonacci_list[i-2] + tribonacci_list[i-3])
         return tribonacci_list
  
+def tribonacci(signature ,n):
+    while len(signature) < n:
+        signature.append(sum(signature[-3:]))
+    
+    return signature[:n]
+
+def tribonacci(signature, n):
+  res = signature[:n]
+  for i in range(n - 3): res.append(sum(res[-3:]))
+  return res
+
+def tribonacci(signature,n):
+    [signature.append(sum(signature[i-3:i])) for i in range(3,n)]
+    return signature[0:n]
