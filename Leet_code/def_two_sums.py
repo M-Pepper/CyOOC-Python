4b85a955eeb22cfac8932ad6a84fcb85ca95f3ad
M-Pepper <46696465+M-Pepper@users.noreply.github.com>
2021-07-30 19:10:05 -0400

    Create def_two_sums.py

---
+++ Leet_code/def_two_sums.py
@@ -0,0 +1,32 @@
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[j] == target - nums[i]:
+                return [i, j]
+                
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    hashmap = {}
+    for i in range(len(nums)):
+        hashmap[nums[i]] = i
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hashmap and hashmap[complement] != i:
+            return [i, hashmap[complement]] 
+              
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    hashmap = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in hashmap:
+            return [i, hashmap[complement]]
+        hashmap[nums[i]] = i
+            
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    dictionary = dict()
+    pos = 0
+    while pos < len(nums):
+        if (target - nums[pos]) not in dictionary:
+            dictionary[nums[pos]] = pos
+            pos += 1
+        else:
+            return [dictionary[target - nums[pos]], pos]
